4a3135288ab5f20f39d697c86097bb364b3ed83f
Pierre Gettant <65826912+StoicInsight@users.noreply.github.com>
2023-04-21 22:21:23 -0400

    factory

---
+++ factory/index.js
@@ -0,0 +1,18 @@
+
+const createUser = ( {firstName, lastName, email} ) => ({
+    firstName,
+    lastName,
+    email, 
+    fullName()  {
+        return `Hello ${this.firstName} ${this.lastName}`
+    }
+
+})  
+
+const user1 = createUser({
+    firstName: "John",
+    lastName: "Doe",
+    email: "john@doe.com"
+});
+
+console.log(user1)
